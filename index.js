5e2d3309a31a29ddf81a3cd49dba15477368bd36
Tony Brix <tony@brix.ninja>
2018-10-01 22:34:18 -0500

    fix versions

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var http = require("http");
 
 var server = http.createServer(function (request, response) {
-
-	response.json(process.versions);
-
+	response.writeHead(200, {"Content-Type": "application/json"});
+	response.end(JSON.stringify(process.versions));
 });
 
 var port = process.env.PORT || 1337;
